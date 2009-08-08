e40e6241a5d1a660c9c09b383229abb1546c48a2
Simon Edwards <simon@simonzone.com>
2009-08-08 20:44:59 +0200

    Moved in the twine preprocessor parser and expression parser/evaluator.

---
+++ exprparser.py
@@ -0,0 +1,339 @@
+#     Copyright 2007-8 Jim Bublitz <jbublitz@nwinternet.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the
+# Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+import ply.lex as lex
+import ply.yacc as yacc
+
+# Operators (+,-,*,/,%,|,&,~,^,<<,>>, ||, &&, !, <, <=, >, >=, ==, !=)
+operators = ('PLUS', 'MINUS', 'SLASH', 'PERCENT', 'VBAR', 'CARET', #'LSHIFT', 'RSHIFT',
+    'LOR', 'LAND', 'BANG', 'LE', 'GE', 'EQ', 'NE',
+    # Increment/decrement (++,--)
+    'PLUSPLUS', 'MINUSMINUS',
+    # Assignment (=, *=, /=, %=, +=, -=, <<=, >>=, &=, ^=, |=)
+    'TIMESEQUAL', 'DIVEQUAL', 'MODEQUAL', 'PLUSEQUAL', 'MINUSEQUAL',
+    'LSHIFTEQUAL',#'RSHIFTEQUAL', 
+    'ANDEQUAL', 'XOREQUAL', 'OREQUAL','EQUALS', 'ASTERISK', 'AMPERSAND', 'TILDE', 'LT', 'GT'
+)
+
+tokens = operators + (
+# Literals (identifier, integer constant, float constant, string constant, char const)
+'ID', 'ICONST', 'OCTCONST', 'HEXCONST', 'FCONST', 'SCONST', 'CCONST',
+
+# Delimeters ( ) [ ] { } , . ; : ::
+'LPAREN', 'RPAREN',
+'LBRACKET', 'RBRACKET',
+'LBRACE', 'RBRACE',
+'COMMA', 'PERIOD', 'SEMI', 'COLON', 'COLON2',
+
+'ARRAYOP', 'FUNCPTR', 'defined' 
+)
+    
+# Completely ignored characters
+t_ANY_ignore           = ' \t\x0c'
+
+# Operators
+t_PLUS             = r'\+'
+t_MINUS            = r'-'
+t_ASTERISK         = r'\*'
+t_SLASH            = r'/'
+t_PERCENT          = r'%'
+t_VBAR             = r'\|'
+t_AMPERSAND        = r'&'
+t_TILDE            = r'~'
+t_CARET            = r'\^'
+#t_LSHIFT           = r'<<'
+#t_RSHIFT           = r'>>'
+t_LOR              = r'\|\|'
+t_LAND             = r'&&'
+t_BANG             = r'!'
+t_LT               = r'<'
+t_GT               = r'>'
+t_LE               = r'<='
+t_GE               = r'>='
+t_EQ               = r'=='
+t_NE               = r'!='
+
+# Assignment operators
+
+t_EQUALS           = r'='
+t_TIMESEQUAL       = r'\*='
+t_DIVEQUAL         = r'/='
+t_MODEQUAL         = r'%='
+t_PLUSEQUAL        = r'\+='
+t_MINUSEQUAL       = r'-='
+t_LSHIFTEQUAL      = r'<<='
+#t_RSHIFTEQUAL      = r'>>='
+t_ANDEQUAL         = r'&='
+t_OREQUAL          = r'\|='
+t_XOREQUAL         = r'^='
+
+# Increment/decrement
+t_PLUSPLUS         = r'\+\+'
+t_MINUSMINUS       = r'--'
+
+# Delimeters
+t_LPAREN           = r'\('
+t_RPAREN           = r'\)'
+t_LBRACKET         = r'\['
+t_RBRACKET         = r'\]'
+t_LBRACE           = r'\{'
+t_RBRACE           = r'\}'
+t_COMMA            = r','
+t_PERIOD           = r'\.'
+t_SEMI             = r';'
+t_COLON            = r':'
+t_COLON2           = r'::'
+
+# Hex Literal
+def t_HEXCONST (t):
+    r'0[x|X][\da-fA-F]+'
+    t.value = eval (t.value)
+    return t
+
+# Octal Literal
+t_OCTCONST = r'0[0-7]{3}?'
+
+# Floating literal
+def t_FCONST (t):
+    r'((\d+)(\.\d+)(e(\+|-)?(\d+))? | (\d+)e(\+|-)?(\d+))([lL]|[fF])?'
+    t.value = float (t.value)
+    return t
+
+# Integer literal
+def t_ICONST (t):
+    r'\d+([uU]|[lL]|[uU][lL]|[lL][uU])?'
+    t.value = int (t.value)
+    return t
+
+# Array operator
+t_ARRAYOP = r'[[].*[]]'
+
+# Function pointer
+t_FUNCPTR = r'\(\s*\*'
+
+# String literal
+t_SCONST = r'\"([^\\\n]|(\\.))*?\"'
+
+# Character constant 'c' or L'c'
+t_CCONST = r'(L)?\'([^\\\n]|(\\.))*?\''
+# Newlines
+
+# def t_CONTINUATION (t):
+#     r'\\\n'
+#     t.lexer.lineno += t.value.count("\n")
+
+def t_ANY_NEWLINE(t):
+    r'\n+'
+    t.lexer.lineno += t.value.count("\n")
+        
+
+def t_ID(t):
+    r'[A-Za-z_][\w_]*'
+    if t.value == 'defined':
+        t.type = 'defined'
+    return t
+
+
+# Comments
+def t_ANY_comment(t):
+    r' /\*(.|\n)*?\*/'
+    t.lineno += t.value.count('\n')
+        
+t_ANY_ignore_cppcomment = '//[^/].*'
+    
+    
+def t_ANY_error(t):
+    print "Illegal character %s" % repr(t.value[0])
+    t.lexer.skip(1)
+
+    
+exprLexer = lex.lex ()
+
+class ExpressionParser(object):
+    precedence = (('left','PLUS','MINUS'), ('left','ASTERISK','SLASH'), ('right','UMINUS'), )
+    
+    def __init__ (self):
+        self.lexer      = exprLexer
+        self.test       = []       
+        self.tokens = tokens        
+        yacc.yacc (module = self, tabmodule = "expressionParserTab")
+        self._parse = yacc.parse
+                   
+        self.values = {}                   
+        
+    def parse (self, s, mode = 'calc', values = None):
+        self.values = values
+        self.lexer.input (s)
+        self.mode = mode
+        self.result = None
+        self._parse (debug = 1, lexer = self.lexer)
+        return self.result
+
+    def p_expression_result (self, p):
+        'expression_result : expression'
+        p [0] = p [1]
+        self.result = p [0]
+
+    def p_expression0 (self, p):
+        """expression : add_expression
+                     | sub_expression
+                     | mult_expression
+                     | div_expression
+                     | mod_expression
+                     | unary_expression
+                     | or_expression
+                     | and_expression
+                     | xor_expression
+                     | bitor_expression
+                     | bitand_expression
+                     | lt_expression
+                     | le_expression
+                     | eq_expression
+                     | ge_expression
+                     | gt_expression
+                     | lshift_expression
+                     | rshift_expression
+                     | ICONST
+                     | FCONST
+                     | HEXCONST"""                    
+        p [0] = p [1]
+            
+    def p_expression1 (self, p):
+        'expression : LPAREN expression RPAREN'
+        p [0] = p [2]
+
+    def p_expression2 (self, p):
+        'expression : ID'
+        if self.mode == 'calc':
+            p [0] = self.values [p [1]]
+        elif self.mode == 'def':
+            p [0] = p [1] in self.values
+    
+    def p_unary_expression (self, p):
+        """unary_expression : sign_expression
+                           | not_expression
+                           | bitnot_expression
+                           | defined_expression"""
+        p [0] = p [1]
+
+    def p_add_expression (self, p):
+        'add_expression : expression PLUS expression'
+        p [0] = p [1] + p [3]
+        
+    def p_sub_expression (self, p):
+        'sub_expression : expression MINUS expression'
+        p [0] = p [1] - p [3]
+    
+    def p_mult_expression (self, p):
+        'mult_expression : expression ASTERISK expression'
+        p [0] = p [1] * p [3]
+    
+    def p_div_expression (self, p):
+        'div_expression : expression SLASH expression'
+        p [0] = p [1] / p [3]
+    
+    def p_mod_expression (self, p):
+        'mod_expression : expression PERCENT expression'
+        p [0] = p [1] % p [3]
+    
+    def p_sign_expression (self, p):
+        'sign_expression : MINUS expression %prec UMINUS'
+        p [0] = - p [2]
+    
+    def p_or_expression (self, p):
+        'or_expression : expression LOR expression'
+        p [0] = p [1] or  p [3]
+    
+    def p_and_expression (self, p):
+        'and_expression : expression LAND expression'
+        p [0] = p [1] and p [3]
+    
+    def p_xor_expression (self, p):
+        'xor_expression : expression CARET expression'
+        p [0] = p [1] ^ p [3]
+    
+    def p_bitor_expression (self, p):
+        'bitor_expression : expression VBAR expression'
+        p [0] = p [1] | p [3]
+    
+    def p_bitand_expression (self, p):
+        'bitand_expression : expression AMPERSAND expression'
+        p [0] = p [1] & p [3]
+    
+    def p_lt_expression (self, p):
+        'lt_expression : expression LT expression'
+        p [0] = p [1] < p [3]
+    
+    def p_le_expression (self, p):
+        'le_expression : expression LE expression'
+        p [0] = p [1] <= p [3]
+    
+    def p_eq_expression (self, p):
+        'eq_expression : expression EQ expression'
+        p [0] = p [1] == p [3]
+    
+    def p_ge_expression (self, p):
+        'ge_expression : expression GE expression'
+        p [0] = p [1] >= p [3]
+    
+    def p_gt_expression (self, p):
+        'gt_expression : expression GT expression'
+        p [0] = p [1] > p [3]
+    
+    def p_lshift_expression (self, p):
+        'lshift_expression : expression GT GT expression'
+        p [0] = p [1] >> p [4]
+    
+    def p_rshift_expression (self, p):
+        'rshift_expression : expression LT LT expression'
+        p [0] = p [1] << p [4]
+
+    def p_not_expression (self, p):
+        'not_expression : BANG expression'
+        p [0] = not p [2]
+
+    def p_bitnot_expression (self, p):
+        'bitnot_expression : TILDE expression'
+        p [0] = ~ p[2]
+
+    def p_defined_expression0 (self, p):
+        'defined_expression : defined ID'
+        p [0] = p[2] in self.values
+
+    def p_defined_expression1 (self, p):        
+        'defined_expression : defined LPAREN ID RPAREN'
+        p [0] = p[3] in self.values
+    
+    def p_error(self, p):
+        print "Syntax error at '%s'" % p.value
+
+                  
+if __name__ == '__main__':                  
+    text = '2 +\
+     2'
+    
+    parser = ExpressionParser ()
+#    exprLexer.input (text)
+#    print parser.parse (text, None)
+    values = {'x': 2.0, 'macro': None}
+    while 1:
+        try:
+            s = raw_input('calc > ')
+        except EOFError:
+            break
+        if not s: continue
+        print parser.parse(s, values)
---
+++ pplexer.py
@@ -0,0 +1,277 @@
+#     Copyright 2007-8 Jim Bublitz <jbublitz@nwinternet.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the
+# Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+import re
+import ply.lex as lex
+# handles the evaluation of conditionals
+from exprparser import ExpressionParser
+
+newtext   = []
+macros    = []
+bitBucket = False
+sentinel  = False
+
+preprocessor_tokens =  ['cond', 'else', 'endif', 'include', 'define', 'undef', 'line', 'error', 'pragma', 'warning']
+tokens = preprocessor_tokens + ['anyline']
+
+values = {}
+evaluate = ExpressionParser ().parse
+
+# Completely ignored characters
+t_ANY_ignore           = ' \t\x0c'
+
+def stripComment (s):
+    pos1 = s.find ('\/\*')
+    pos2 = s.find ('\/\/')
+    if pos1 > 0 and pos2 > 0:
+        pos = min (pos1, pos2)
+    elif pos1 < 0 and pos2 < 0:
+        pos = -1
+    else:
+        pos = max (pos1, pos2)
+    
+    if pos > 0:
+        return s [:pos].strip (), s[pos:].strip ()
+    else:
+        return s, ''
+
+def t_cond (t):
+    r'\#\s*(?P<ifType>ifdef\s|ifndef\s|if\s|elif\s)\s*(?P<cond>.*?)\n'
+
+    # All conditionals that perform a test are handled here
+    global newtext
+    ifType = t.lexer.lexmatch.group ('ifType').strip ()
+    condition, comment  = stripComment (t.lexer.lexmatch.group ('cond'))
+    
+    # #if/#elif look for True/False, others for defintion only
+    # #if defined - 'defined' is handled as an operator by the
+    # expression parser which evaluates the conditional
+    if ifType in ['if', 'elif']:
+        mode = 'calc'
+    else:
+        mode = 'def'
+
+    ifCondition = evaluate (condition, mode, values)
+        
+    global bitBucket, sentinel
+    bitBucket = ((not ifCondition) and (ifType != 'ifndef')) or (ifCondition and (ifType == 'ifndef'))
+    
+    # remove #define <sentinel>?
+    sentinel = not bitBucket and ('_h' in condition or '_H' in condition)
+
+    # A multiline comment could begin on a preprocessor line
+    # that's being eliminated here
+    if bitBucket and comment:
+        newtext.append (comment + '\n')
+    else:
+        newtext.append ('\n')
+
+    t.lexer.lineno += 1
+    
+def t_else (t):
+    r'\#\s*else(.*?)\n'  # comments?
+    global bitBucket, newtext
+    bitBucket = not bitBucket
+    t.lexer.lineno += 1
+    newtext.append ('\n')
+    
+def t_endif (t):
+    r'\#\s*endif(.*?)\n'
+    global bitBucket, newtext
+    bitBucket = False
+    t.lexer.lineno += 1
+    newtext.append ('\n')
+
+def t_include (t):
+    r'\#\s*include.*?\n'
+    global newtext
+    t.lexer.lineno += 1
+    newtext.append ('\n')
+    
+def t_line (t):
+    r'\#\s*line.*?\n'
+    global newtext
+    t.lexer.lineno += 1
+    newtext.append ('\n')
+
+def t_error (t):    
+    r'\#\s*error.*?\n'
+    global newtext
+    t.lexer.lineno += 1
+    newtext.append ('\n')
+    
+def t_pragma (t):    
+    r'\#\s*pragma.*?\n'
+    global newtext
+    t.lexer.lineno += 1
+    newtext.append ('\n')
+    
+def t_warning (t):    
+    r'\#\s*warning.*?\n'
+    global newtext
+    t.lexer.lineno += 1
+    newtext.append ('\n')
+
+def t_undef (t):
+    r'\#\s*undef\s*(?P<item>.*?)\n'
+    global macros, values, newtext
+    item = t.lexer.lexmatch.group ('item').strip ()
+    if item in values:
+        macros = [macro for macro in macros if macro [2] != item]
+        del values [item]
+    t.lexer.lineno += 1
+    newtext.append ('\n')
+    
+def t_define (t):
+    r'\#\s*define\s*(?P<first>[\S]+)\s*?(?P<second>[^\n]*?)\n'    
+    global sentinel, values, macros, newtext
+    a = t.lexer.lexmatch.group ('first')
+    b = t.lexer.lexmatch.group ('second')
+    
+    # Append any continuation lines
+    newlines = 1
+    start = t.lexer.lexpos
+    if b and b.endswith ('\\'):              
+        data = t.lexer.lexdata
+        for i in range (start, len (data)):
+            if data [i] == '\n':
+                t.lexer.lineno += 1
+                newlines += 1
+            if data [i] == '\n' and data [i - 1] != '\\':                
+                break
+        t.lexer.lexpos = i + 1
+        b += data [start:t.lexer.lexpos].replace ('\\\n', ' ')
+
+    if '(' in a and not ')' in a:
+        pos = b.find (')')
+        if pos < 0:
+            return
+        a += b [:pos + 1]
+        b = b [pos + 1:]
+    
+    # remove #define <sentinel>
+    sentinel = sentinel and not b and ('_h' in a or '_H' in a)
+    if not sentinel:
+        if not b or '(' in a:
+            values [a] = ''
+            macros.insert (0, (re.compile (a), '', a))
+        else:
+            values [a] = b
+            macros.insert (0, (re.compile (a), b.strip (), a))
+    
+    sentinel = False
+            
+    newtext.append (newlines *'\n')
+    t.lexer.lineno += 1
+    
+def t_anyline (t):
+    r'.*?\n'
+    
+    """
+    Process anything that's not a preprocesor directive.
+
+    Apply all #define macros to each line. Code that has
+    been #if'd out (bitBucket == True) is replaced by
+    a single newline for each line removed.
+    """
+    global sentinel, newtext
+    sentinel = False
+    if not bitBucket:
+        line = t.value
+        for m in macros:
+            line = m [0].sub (m [1], line)
+        newtext.append (line)
+    else:
+        newtext.append ('\n')
+        
+    t.lexer.lineno += 1   
+
+# this needs to be HERE - not above token definitions
+ppLexer = lex.lex ()
+
+    
+def preprocess (text, global_values, global_macros):
+    """
+    Preprocesses h files - does #define substitutions and
+    evaluates conditionals to include/exclude code. No
+    substitutions are performed on preprocessor lines (any
+    line beginning with '#'). Global #defines are applied
+    LAST, so they override any local #defines.
+
+    All C preprocessor code is stripped, and along with any
+    lines eliminated conditionally, is replaced with newlines
+    so that error messages still refer to the correct line in
+    the original file.
+    """
+    global newtext, bitBucket, macros, values
+    newtext   = []
+    bitBucket = False
+    macros    = [] + global_macros
+    values    = {}
+        
+    values.update (global_values)      
+
+    ppLexer.input (text)
+
+    token = ppLexer.token ()
+
+    return "".join (fixDoc (newtext))
+    
+
+def fixDoc (textList):
+    doReplace = False
+    doBackReplace = False
+    nLines    = len (textList)
+    for i in range (nLines):
+        if i >= nLines - 1:
+            break
+            
+        if textList [i].startswith ('/////'):
+            textList [i] = '\n'
+            continue
+            
+        haveBackCmt = textList [i].find ('///<') >= 0
+        haveCmt = textList [i].find ('///') >= 0 and not haveBackCmt
+        if haveBackCmt:
+            if not doBackReplace:
+                doBackReplace = textList [i + 1].strip ().startswith ('///<')
+                if doBackReplace:
+                    textList [i] = textList [i].replace ('///<', '/**<')
+            else:
+                textList [i] = textList [i].replace ('///<', '*')            
+        elif doBackReplace:
+            textList.insert (i, '*/\n')
+            doBackReplace = False
+            
+        if not haveBackCmt and haveCmt:
+            if not doReplace:
+                doReplace = textList [i + 1].strip ().startswith ('///')
+                if doReplace:
+                    textList [i] = textList [i].replace ('///', '/**')
+            else:
+                textList [i] = textList [i].replace ('///', '*')
+        elif doReplace:
+            textList.insert (i, '*/\n')
+            doReplace = False
+            
+    return textList
+            
+        
+    
+    
+if __name__ == '__main__':
+    text = """#define foo bar"""

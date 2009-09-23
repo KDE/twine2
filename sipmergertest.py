7179a93046b17451c1e987a24a7ae68043ca810b
Simon Edwards <simon@simonzone.com>
2009-09-23 22:27:49 +0200

    Start of the Sip file merger.

---
+++ sipmergertest.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+#     Copyright 2009 Simon Edwards <simon@simonzone.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the
+# Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+import unittest
+import sipmerger
+
+class TestSipMerger(unittest.TestCase):
+    def merge(self, oldSipText, newSipText):
+        parser = sipparser.SipParser()
+        syms = sipsymboldata.SymbolData()
+        oldScope = parser.parse(syms,oldSipText)
+        newScope = parser.parse(syms,newSipText)
+        
+        print("Old Sip -------------------------------------")
+        print(oldScope.format())
+        print("New Sip -------------------------------------")
+        print(newScope.format())
+        
+        sipmerger.MergeSipScope(oldScope,newScope)
+        
+        print("Result --------------------------------------")
+        print(newScope.format())
+
+    def testMerge(self):
+        self.merge("""
+void foo();
+        ""","""
+void foo();
+        """)
+
+if __name__ == '__main__':
+    unittest.main()

09b8480eeb9e0be9b6ae020345745092a85473be
Simon Edwards <simon@simonzone.com>
2010-06-20 22:20:12 +0200

    API Docs: Show QPairs as Python tuples (x,y) in the docs.

--- toolkit.py
+++ toolkit.py
@@ -1267,6 +1267,12 @@ code fragments in the documentation have not been translated from C++ to Python.
             leftType = self.linkType(typeName[5:pivot],context)
             rightType = self.linkType(typeName[pivot+1:-1],context)
             return '{' + leftType + ':'+ rightType + '}'
+            
+        if typeName.startswith("QPair<"):
+            pivot = typeName.index(",")
+            leftType = self.linkType(typeName[6:pivot],context)
+            rightType = self.linkType(typeName[pivot+1:-1],context)
+            return '(' + leftType + ','+ rightType + ')'
 
         if typeName.startswith("Qt.") or typeName.startswith("Qt::"):
             return '<a href="http://www.riverbankcomputing.co.uk/static/Docs/PyQt4/html/qt.html">' + typeName + '</a>'

14e27c3c482a75d750c938e8f9045eedc733d8df
Simon Edwards <simon@simonzone.com>
2014-10-19 08:31:26 +0200

    Improved quote handling in the cmake lexer.

--- cmakelexer.py
+++ cmakelexer.py
@@ -18,14 +18,18 @@ class CMakeLexerClass(object):
     tokens = tokens
 
     t_ANY_ignore = " \t"
-    t_SYMBOL = r'[^()\s#"]+'
+    def t_SYMBOL(self, t):
+        r'[^\\()\s#"]+(\\"[^()\s#"\\]*)*'
+        t.value = t.value.replace(r'\"', '"')
+        return t
+
     t_LPAREN = r'\('
     t_RPAREN = r'\)'
     t_ANY_COMMENT = r'\#.*'
 
     def t_STRING(self, t):
         r'"[^"\\]*(\\"[^"\\]*)*"'
-        t.value  = t.value[1:-1]
+        t.value  = t.value[1:-1].replace(r'\"', '"')
         t.lexer.lineno += t.value.count("\n")
         return t
 

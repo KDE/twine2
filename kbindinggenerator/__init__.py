f705e93e5e546b909d20711a9e6d9d7d5522704c
Scott Kitterman <scott@kitterman.com>
2015-04-19 16:03:14 -0400

    Add copyright/licensing information and package description to kbindinggenerator/__init__.py

--- kbindinggenerator/__init__.py
+++ kbindinggenerator/__init__.py
@@ -1,6 +1,22 @@
+# kbinding generator: Generate sip based Python bindings for C++
+
 # This file is part of the kbindinggenerator project for generating Python
 # bindings for C++ programs, particularly KDE.
-#
+
+# argvalidate.py: Copyright (C) 2009 Stephan Peijnik (stephan@peijnik.at)
+# https://bitbucket.org/sp/python-argvalidate
+# License: LGPL-3+, see COPYING.LESSER
+
+#Other files (except siplexer.py and __init__.py): 
+#Copyright 2007- 2014 Simon Edwards <simon@simonzone.com>
+#License: GPL-2+
+
+#cpplexer.py, cppparser.py, cppsymboldata.py, exprparser.py, pplexer.py,
+#reducetxt.py, siplexer.py, and sipparser.py:
+#Copyright 2007-8 Jim Bublitz <jbublitz@nwinternet.com>
+#License: GPL=2+
+
+# __init__.py, this file:
 # Copyright © 2015 Scott Kitterman <scott@kitterman.com>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy

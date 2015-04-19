0a7d0bc60f5df9a949f62e43b8f62b8254db210e
Scott Kitterman <scott@kitterman.com>
2015-04-19 14:44:10 -0400

    Add __init__.py.

---
+++ kbindinggenerator/__init__.py
@@ -0,0 +1,56 @@
+# This file is part of the kbindinggenerator project for generating Python
+# bindings for C++ programs, particularly KDE.
+#
+# Copyright © 2015 Scott Kitterman <scott@kitterman.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+# __init__.py for kbindinggenerator class.
+
+__version__ = '0.1'
+
+from . import argvalidate.py
+from . import cmakeparsertest.py
+from . import cpplexer.py
+from . import cppsymboldata.py
+from . import exprparser.py
+from . import pplexer.py
+from . import qtkdemacros.py
+from . import siplexer.py
+from . import sipparser.py
+from . import toolkit.py
+from . import cmakelexer.py
+from . import cmake.py
+from . import cppparser.py
+from . import cpptosiptransformer.py
+from . import pplexertest.py
+from . import reducetxt.py
+from . import sipmerger.py
+from . import sipparsertest.py
+from . import cmakeparser.py
+from . import cpp_grammer.txt
+from . import cppparsertest.py
+from . import cpptosiptransformertest.py
+from . import kggzmod.py
+from . import qtkde5macros.py
+from . import sealed.py
+from . import sipmergertest.py
+from . import sipsymboldata.py
+
+
